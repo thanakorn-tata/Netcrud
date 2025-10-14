a69a4c9fcfdacce7c10f9ed274461153b3a4d176
thanakorn-tata <thanakornkhomkham@gmail.com>
2025-10-14 10:22:01 +0700

    student manage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { StudentComponent } from './mastermenu/student/student/student.component';
-import { BudgetComponent } from './mastermenu/student/budget/budget.component';
-import { RestaurantComponent } from './mastermenu/student/restaurant/restaurant.component';
+// import { BudgetComponent } from './mastermenu/student/budget/budget.component';
+// import { RestaurantComponent } from './mastermenu/student/restaurant/restaurant.component';
 
 export const routes: Routes = [
     { path: '', component: LayoutMainComponent,
@@ -11,8 +11,8 @@ export const routes: Routes = [
         { path: '', redirectTo: 'Dashboard', pathMatch: 'full' },
         { path: 'Dashboard', component: DashboardComponent },
         { path: 'student', component: StudentComponent },
-        { path: 'budget', component: BudgetComponent },
-        { path: 'restaurant', component: RestaurantComponent }
+        // { path: 'budget', component: BudgetComponent },
+        // { path: 'restaurant', component: RestaurantComponent }
 
       ]
     },
---
+++ src/app/mastermenu/student/student-manage/student-manage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-manage',
+  imports: [],
+  templateUrl: './student-manage.component.html',
+  styleUrl: './student-manage.component.scss'
+})
+export class StudentManageComponent {
+
+}
